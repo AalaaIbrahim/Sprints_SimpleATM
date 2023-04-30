1c73fe4321781ad2b0f52b439feef41467d14cda
sharpel malak <62040665+sharpelmalak@users.noreply.github.com>
2023-04-30 17:57:17 +0300

    adding usart and husart modules

--- Project/CARD_MCU/Debug/main.d
+++ Project/CARD_MCU/Debug/main.d
@@ -1,15 +1,1 @@
-main.d main.o: .././main.c .././Common/BIT_Math.h .././Common/STD_Types.h \
- .././Common/vect_table.h .././MCAL/dio/dio_interface.h \
- .././MCAL/spi/spi_interface.h .././ECUAL/hspi/hspi_interface.h
-
-.././Common/BIT_Math.h:
-
-.././Common/STD_Types.h:
-
-.././Common/vect_table.h:
-
-.././MCAL/dio/dio_interface.h:
-
-.././MCAL/spi/spi_interface.h:
-
-.././ECUAL/hspi/hspi_interface.h:
+main.d main.o: .././main.c
--- Project/CARD_MCU/Debug/makedep.mk
+++ Project/CARD_MCU/Debug/makedep.mk
@@ -4,9 +4,13 @@
 
 ECUAL\hspi\hspi_program.c
 
+ECUAL\husart\husart.c
+
 main.c
 
 MCAL\dio\dio_prog.c
 
 MCAL\spi\spi_program.c
+
+MCAL\usart\usart.c
 
